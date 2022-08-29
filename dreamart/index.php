3e4d227551f922bd60d38f72b7121ad59cc09739
Baljeet1506 <106123418+Baljeet1506@users.noreply.github.com>
2022-08-28 17:36:18 -0700

    Add files via upload

---
+++ dreamart/index.php
@@ -0,0 +1,253 @@
+<html>
+<head>
+<title> Dream Art </title>
+
+<style>
+.nict {
+float: left;
+margin: 0px;
+padding: 1px;
+width: 220px;
+height: 250px;
+border:0px solid black;
+}
+
+.nict1 {
+float: right;
+margin: 0px;
+padding: 1px;
+width: 745px;
+height: 250px;
+border:0px solid black;
+} 
+
+
+
+.sample1 {
+float: left;
+margin: 0px;
+padding: 1px;
+width: 230px;
+height: 480px;
+border:0px solid black;
+}
+
+.sample2 {
+float: right;
+margin: 0px;
+padding: 1px;
+width: 750px;
+height: 480px;
+border:0px solid black;
+}
+
+</style>
+
+
+</head>
+<body background="pages/images/bg4.png">
+
+<table align=center width=75% > 
+<tr>
+	<td height=120 BACKGROUND="pages/images/bg1.jpg" VALIGN="top">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+		&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+		<font size="3" face=arial><b><a href="Admission.html" style="color: #0B2F3A;text-decoration : none ">Join Us	|</a>
+		<a href="Contact.html" style="color: #0B2F3A;text-decoration : none ">Contact Us</a></b></font>
+	</td>
+</tr>
+<tr>
+	<td height=20 BACKGROUND="pages/images/bgc.jpg">
+	</td>	
+</tr>
+</table>
+
+<table align=center width=75% >
+<tr>
+<td> 
+	<div class="nict" > 
+	<table width=100% height=100%>
+		<tr align=center>
+			<td> <font face="Andalus" size=3><b><a href="DreamArt.html" style="color: #0B2F3A;text-decoration : none "> Home </a> <hr style="height:1px;color:#0B2F3A;background-color:#0B2F3A;width:80%"> </td>
+		</tr>
+		<tr align=center>
+			<td> <font face="Andalus" size=3><b><a href="Paintings.html" style="color: #0B2F3A;text-decoration : none "> Paintings</a> <hr style="height:1px;color:#0B2F3A;background-color:#0B2F3A;width:80%"> </td>
+		</tr>
+		<tr align=center>
+			<td> <font face="Andalus" size=3><b><a href="PhotoGallery.html" style="color: #0B2F3A;text-decoration : none "> Photo Gallery </a><hr style="height:1px;color:#0B2F3A;background-color:#0B2F3A;width:80%"> </td>
+		</tr>
+		<tr align=center>
+			<td> <font face="Andalus" size=3><b><a href="Admission.html" style="color: #0B2F3A;text-decoration : none "> Join Us </a><hr style="height:1px;color:#0B2F3A;background-color:#0B2F3A;width:80%"> </td>
+		</tr>
+		<tr align=center>
+			<td> <font face="Andalus" size=3><b><a href="Contact.html" style="color: #0B2F3A;text-decoration : none "> Contact </a><hr style="height:1px;color:#0B2F3A;background-color:#0B2F3A;width:80%"></td>
+		</tr>
+
+		
+	</table>
+	</div>
+	
+
+	<div class="nict1">
+	<img src="pages/images/008.jpg" height=250px width=745px>	
+	</div>	
+	
+</td>
+</tr>
+</table>
+
+
+
+
+<table width=75% align=center>
+<tr>
+<td>
+	<div class="sample1">
+		<table width=90% height=100%>
+		<tr>
+		<td>
+		<div class="nict">
+			<table height=100% width=100%>
+			
+			<tr>
+			<td align=center BACKGROUND="pages/images/bgc.jpg" VALIGN="center" height=20%> 
+			<font size="5" color=white face="Snap ITC"><b>Art Room</b></font></td>
+			</tr>
+			
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg"> 
+			<font size="2" face="arial"><a href="Art&Craft.html" style="color: black;text-decoration : none ">Art & Craft </a></td>
+			</tr>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg"> 
+			<font size="2" face="arial"><a href="3D_Designs.html" style="color: black;text-decoration : none "> 3D/Canvas/Emboss Paintings </a></td>
+			</tr>
+
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg"> 
+			<font size="2" face="arial"><a href="Paintings.html" style="color: black;text-decoration : none ">Paintings</a></td>
+			</tr>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg"> 
+			<font size="2" face="arial"><a href="Greeting.html" style="color: black;text-decoration : none ">Greeting Cards</a></td>
+			</tr>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg"> 
+			<font size="2" face="arial"><a href="Designs.html" style="color: black;text-decoration : none ">Designs</a></td>
+			</tr>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg"> 
+			<font size="2" face="arial"><a href="Sketches.html" style="color: black;text-decoration : none ">Sketches</a></td>
+			</tr>
+			
+			</table>
+		</div>
+		</td>
+		</tr>
+		
+		<tr>
+		<td>
+		<div class="nict">
+			<table height=100% width=100%>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bgc.jpg" VALIGN="center" height=20%> 
+			<font size="5" color=white face="Snap ITC"><b>Contact Us</b></font></td>
+			</tr>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg"> 
+			<font size="3" face="arial"><b>Address:</b></font><br> 77 Guru Nanak Pura (West),<br> Jalandhar 144009</td>
+			</tr>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg">
+			<font size="3" face="arial"><b>Phone:</b></font><br>0181-5011014</td>
+			</tr>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg">
+			<font size="3" face="arial"><b>Mobile:</b></font><br> +919888-60-5077</td>
+			</tr>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bg.jpg"> 
+			<font size="3" face="arial"><b>Email:</b></font><br>jatinmehra5077@gmail.com</td>
+			</tr>
+			</table>
+		</div>
+		</td>
+		</tr>
+		</table>
+
+	</div>
+	
+
+
+	<div class="sample2"> 
+		<table width=100% height=100%>
+		<tr>
+		<td>
+			<div class="nict1">
+			<table height=100% width=100%>
+			<tr>
+			<td align=center BACKGROUND="pages/images/bgc.jpg" VALIGN="center" height=20%> 
+			<font size="6" color=white face="Snap ITC"><b>Welcome To Dream Art</b></font></td>
+			</tr>
+			<tr height=80%>
+			<td align=justice><marquee direction=up scrollamount=2 onMouseover="this.stop()" onMouseOut="this.start()"> <font face=Gabriola color=#0B2F3A size=4>
+			Art is all about imagination, creativity, and talent. DreamArt is a place that provides you with a platform to create and explore. You can also buy paintings, crafted things, handmade greeting cards etc. If you want your own designs on greetings, we can do that also. you just have to send us the photos and matter according to your requirements.
+			<p> If you want to sell your products, like greeting cards or paintings or designs, or want them to be used by us, then also you can contact us. Come fast then and grab the opportunity :)</p> </font></marquee></td>
+	
+			</tr>
+			</table>
+			</div>
+		</td>
+		</tr>
+			
+		<tr>
+		<td>
+			<div class="nict1"> 
+				<table height=100% width=100%>
+				<tr>
+				<td colspan=3 align=center BACKGROUND="pages/images/bgc.jpg" VALIGN="center" height=20%> 
+				<font size="6" color=white face="Snap ITC"><b>Photo Gallery</b></font></td>
+				</tr>
+				<tr height=80%>
+				<td> <img border=1 src="pages/images/teddy.jpg" height=160 width=230>
+				</td>
+				<td> <img border=1 src="pages/images/3D.jpg" height=160 width=260>
+				</td>
+				<td> <img border=1 src="pages/images/pot.jpg" height=160 width=230>
+				</td> 
+
+				</tr>
+				</table>
+			</div>
+		</td>
+		</tr>
+		</table>
+	</div>
+</td>
+</tr>
+</table>
+
+
+<br>
+<br>
+<br>
+
+<hr style="height:1px;color:#333;background-color:#333; width:75%">
+<table width=75% align=center>
+<tr>
+	<td>
+	<font size=2> Copyright &copy;2014 NICT Computer's All Rights reserved<br>
+	Website Designed & Developed By NICT Comp.(Regd.) Visit Us At  &nbsp; &nbsp;<a href"a href="http://nictcomputers.com/">nictcomputers.com</font>
+	</td>
+</tr>
+</table>
+</body>
+</html>
+
+
+
+
+
+
+
+
